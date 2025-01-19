88f76aa7e2f4b753cdd2569c1eb34d14b0c72033
Leonard Nania <leonard.nania@media-meter.com>
2025-01-19 22:22:48 +0800

    UPDATE portfolio

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faGithub, faLinkedin, faAws, faDocker } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faLocationDot, faMobileScreenButton, faHouse, faCode, faLaptopCode, faPhone } from '@fortawesome/free-solid-svg-icons'
-library.add(faGithub, faAws, faDocker, faLinkedin, faEnvelope, faLocationDot, faMobileScreenButton, faHouse, faCode, faLaptopCode, faPhone)
+import { faLocationDot, faMobileScreenButton, faHouse, faCode, faLaptopCode, faPhone, faDharmachakra } from '@fortawesome/free-solid-svg-icons'
+library.add(faGithub, faAws, faDocker, faLinkedin, faEnvelope, faLocationDot, faMobileScreenButton, faHouse, faCode, faLaptopCode, faPhone, faDharmachakra)
 
 import axios from 'axios'
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND;
